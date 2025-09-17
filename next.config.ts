1536e643a80a75c7b29000d8dacfbddb3b0fefa6
kanakana-kn87 <kanakana.kn87@gmail.com>
2025-09-17 19:22:52 +0900

    スピナーを使ったよ！

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { join } from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': join(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { TailSpin } from 'react-loader-spinner';
+
+export default function SpinnerComponent() {
+  return (<TailSpin color="#dfdfdfcb" height={80} width={80} />
+  );
+}
